677a29778a1390ef1e0d83975d64e6fc3c01f695
Bikrant Bikram Pratap Maurya <48064909+bikkimaurya@users.noreply.github.com>
2024-08-21 11:52:05 +0530

    Second Commit

---
+++ Practice/shift_cipher.py
@@ -0,0 +1,55 @@
+# sub = [-1 for i in range(128)]
+# sub = {}
+# print("Chose Option\n")
+# print("\t 1. Enter Shift Value \n")
+# print("\t 2. Default")
+# n =input(" Enter Shift Value")
+
+# def shift(n):
+#     for i in range(128):
+#         sub[i] = (i+n)%128
+
+def encrypt(plainText, n):
+    # shift(n)
+    # print(sub)
+    cipher = ""
+    for i in plainText:
+        cipher += chr(ord(i)+n)
+    return cipher
+
+def decrypt(cipher, n):
+    plainData = ""
+    for i in cipher:
+        val = ord(i)-n
+        if(val <0):
+            val+=128
+        plainData+=chr(val)
+    return plainData
+
+
+
+while(True):
+    print("What do u want to perform : ")
+    print("\t 1. Encrypt")
+    print("\t 2. Decrypt")
+    print("\t 3. Exit ( Default )")
+
+    n = int(input("Enter Your responce : "))
+    if( n == 1):
+        key = int(input("Enter your Shift Key : "))
+        plainText = input("Enter your data which you want to Encrypt : ")
+        cipher = encrypt(plainText, key)
+        print("Your Encrypted data is ")
+        print("-----------------------------Chipher -----------------------")
+        print(cipher)
+        print("--------------------------------------------------------------")
+    elif( n == 2):
+        key = int(input("Enter your Shift Key : "))
+        cipher = input("Enter your data which you want to Decrypt : ")
+        plainText = decrypt(cipher, key)
+        print("Your Decrypted data is ")
+        print("-----------------------------Data -----------------------")
+        print(plainText)
+        print("--------------------------------------------------------------")
+    else:
+        break
